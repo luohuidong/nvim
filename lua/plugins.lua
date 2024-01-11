3b69f06e9c95a01fe1d00ef0cf5276c40de8801a
luohuidong <luohuidong01@126.com>
2024-01-11 21:46:26 +0800

    config nvim.cmp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -61,10 +61,13 @@ require("lazy").setup({
             })
         end
     },
-    {
-        "neovim/nvim-lspconfig"
-    },
-    {
-        "williamboman/mason.nvim"
-    }
+    { "neovim/nvim-lspconfig" },
+    { "williamboman/mason.nvim" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+    { "hrsh7th/cmp-cmdline" },
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-vsnip" },
+    { "hrsh7th/vim-vsnip" }
 })
