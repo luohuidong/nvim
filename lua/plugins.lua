c3bf2f3d23450fa5be9e6a9e2b6f0f28807448aa
luohuidong <luohuidong01@126.com>
2024-01-12 03:46:06 +0800

    config telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -64,11 +64,15 @@ require("lazy").setup({
     { "williamboman/mason.nvim" },
     { "williamboman/mason-lspconfig.nvim" },
     { "neovim/nvim-lspconfig" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/cmp-buffer" },
-    { "hrsh7th/cmp-path" },
-    { "hrsh7th/cmp-cmdline" },
-    { "hrsh7th/nvim-cmp" },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "hrsh7th/cmp-cmdline" }
+    },
     { "hrsh7th/cmp-vsnip" },
-    { "hrsh7th/vim-vsnip" }
+    { "hrsh7th/vim-vsnip" },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-fzf-native.nvim' }
+    }
 })
