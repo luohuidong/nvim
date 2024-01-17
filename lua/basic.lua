7e2f9e6151c86aaa420cea8e02ebc072cb5aaf16
luohuidong <luohuidong01@126.com>
2024-01-17 08:37:40 +0800

    remove vim.o.listchars config

--- lua/basic.lua
+++ lua/basic.lua
@@ -67,7 +67,6 @@ vim.o.termguicolors = true
 vim.opt.termguicolors = true
 -- 不可见字符的显示，这里只把空格显示为一个点
 vim.o.list = true
-vim.o.listchars = "space:·"
 -- 补全增强
 vim.o.wildmenu = true
 -- Dont' pass messages to |ins-completin menu|
