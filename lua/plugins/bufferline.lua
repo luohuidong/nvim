3f2e146930224a4905dcb13712cc7c4960a240f4
luohuidong <luohuidong01@126.com>
2024-01-18 02:28:24 +0800

    config bufferline offsets

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -6,6 +6,18 @@ return {
     config = function()
         vim.opt.termguicolors = true
 
-        require("bufferline").setup {}
+        require("bufferline").setup {
+            options = {
+                offsets = {
+                    {
+                        filetype = "NvimTree",
+                        text = "File Explorer",
+                        highlight = "Directory",
+                        separator = true -- use a "true" to enable the default, or set your own character
+                    }
+                }
+
+            }
+        }
     end
 }
