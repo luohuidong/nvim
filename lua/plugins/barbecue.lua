f4848f9adcef370a6050e9c82d6d29994b72f3e9
luohuidong <luohuidong01@126.com>
2024-01-17 15:44:04 +0800

    update barbecue config

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -6,7 +6,28 @@ return {
         "SmiteshP/nvim-navic",
         "nvim-tree/nvim-web-devicons", -- optional dependency
     },
-    opts = {
-        theme = 'tokyonight'
-    },
+    config = function()
+        -- triggers CursorHold event faster
+        vim.opt.updatetime = 200
+
+        require("barbecue").setup({
+            create_autocmd = false, -- prevent barbecue from updating itself automatically
+            theme = 'tokyonight'
+        })
+
+        vim.api.nvim_create_autocmd({
+            "WinScrolled", -- or WinResized on NVIM-v0.9 and higher
+            "BufWinEnter",
+            "CursorHold",
+            "InsertLeave",
+
+            -- include this if you have set `show_modified` to `true`
+            "BufModifiedSet",
+        }, {
+            group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+            callback = function()
+                require("barbecue.ui").update()
+            end,
+        })
+    end
 }
