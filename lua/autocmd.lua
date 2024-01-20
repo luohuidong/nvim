037d37034114b2f8eff111743d581818a4c30816
luohuidong <luohuidong01@126.com>
2024-01-20 12:58:40 +0800

    update auto command

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,23 +1,9 @@
 -- https://neovim.io/doc/user/lua-guide.html#lua-guide-autocommands
 
-local autocmd = vim.api.nvim_create_autocmd
-
-autocmd("TermOpen", {
-    command = "startinsert",
-})
-
-autocmd({ "InsertLeave", "TextChanged" }, {
-    command = "Format",
-})
-
-autocmd({ "FocusLost" }, {
-    command = "w",
-})
-
-autocmd("BufEnter", {
-    pattern = { "*.md" },
-    callback = function()
-        vim.wo.colorcolumn = ""
-        vim.o.wrap = true
-    end,
-})
+vim.cmd([[
+    augroup FormatAutogroup
+        autocmd!
+        autocmd FocusLost * Format
+        autocmd User FormatterPost w
+    augroup END
+]])
