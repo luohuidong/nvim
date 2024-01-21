7af1c35bb217eebade97e37dd60009b74ce1bb20
luohuidong <luohuidong01@126.com>
2024-01-22 02:00:05 +0800

    format code

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,19 +1,19 @@
 -- https://github.com/nvim-telescope/telescope.nvim
 
-local telescope_keybindings = require('keybindings.telescope-kb')
+local telescope_keybindings = require("keybindings.telescope-kb")
 
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
     dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-telescope/telescope-fzf-native.nvim',
-        'nvim-telescope/telescope-project.nvim'
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-fzf-native.nvim",
+        "nvim-telescope/telescope-project.nvim",
     },
     config = function()
-        local telescope = require('telescope')
-        telescope.load_extension('project')
+        local telescope = require("telescope")
+        telescope.load_extension("project")
 
         telescope_keybindings.basic()
-    end
+    end,
 }
