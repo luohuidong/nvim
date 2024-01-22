a7e3e6cf5dfd4c4385575021b2fd0615e6530ade
luohuidong <luohuidong01@126.com>
2024-01-22 20:24:08 +0800

    format the code

--- lua/keybindings/telescope-kb.lua
+++ lua/keybindings/telescope-kb.lua
@@ -6,8 +6,12 @@ function M.basic()
     local builtin = require("telescope.builtin")
 
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Lists files in your current working directory" })
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep,
-        { desc = "Search for a string in your current working directory" })
+    vim.keymap.set(
+        "n",
+        "<leader>fg",
+        builtin.live_grep,
+        { desc = "Search for a string in your current working directory" }
+    )
     vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Lists open buffers in current neovim instance" })
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Lists available help tags" })
 end
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,8 +1,8 @@
 -- https://github.com/nvim-lualine/lualine.nvim
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require('lualine').setup()
-    end
+        require("lualine").setup()
+    end,
 }
