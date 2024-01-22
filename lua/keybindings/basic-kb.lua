0a4bafb9c716a0c673ad47e9848acd996810a669
luohuidong <luohuidong01@126.com>
2024-01-22 20:55:28 +0800

    modify keybindings

--- lua/keybindings/basic-kb.lua
+++ lua/keybindings/basic-kb.lua
@@ -1,36 +1,35 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-local map = vim.api.nvim_set_keymap
-
-local function set_opt(opt)
+local map = function(mode, lhs, rhs, opt)
     opt = opt or {}
     opt.noremap = opt.noremap or true
     opt.silent = opt.silent or true
-    return opt
+
+    vim.api.nvim_set_keymap(mode, lhs, rhs, opt)
 end
 
-map("n", "<leader>sv", ":vsp<CR>", set_opt({ desc = "split window vertically" }))
-map("n", "<leader>sh", ":sp<CR>", set_opt({ desc = "split window horizontally" }))
-map("n", "<leader>sc", "<C-w>c", set_opt({ desc = "close current window" }))
-map("n", "<leader>so", "<C-w>o", set_opt({ desc = "close other windows" }))
+map("n", "<leader>sv", ":vsp<CR>", { desc = "split window vertically" })
+map("n", "<leader>sh", ":sp<CR>", { desc = "split window horizontally" })
+map("n", "<leader>sc", "<C-w>c", { desc = "close current window" })
+map("n", "<leader>so", "<C-w>o", { desc = "close other windows" })
 
 -- window navigation
-map("n", "<A-h>", "<C-w>h", set_opt({ desc = "jump to left window" }))
-map("n", "<A-j>", "<C-w>j", set_opt({ desc = "jump to down window" }))
-map("n", "<A-k>", "<C-w>k", set_opt({ desc = "jump to up window" }))
-map("n", "<A-l>", "<C-w>l", set_opt({ desc = "jump to right window" }))
+map("n", "<A-h>", "<C-w>h", { desc = "jump to left window" })
+map("n", "<A-j>", "<C-w>j", { desc = "jump to down window" })
+map("n", "<A-k>", "<C-w>k", { desc = "jump to up window" })
+map("n", "<A-l>", "<C-w>l", { desc = "jump to right window" })
 
 -- resize window
-map("n", "<C-Left>", ":vertical resize -2<CR>", set_opt())
-map("n", "<C-Right>", ":vertical resize +2<CR>", set_opt())
-map("n", "<C-Down>", ":resize +2<CR>", set_opt())
-map("n", "<C-Up>", ":resize -2<CR>", set_opt())
+map("n", "<C-Left>", ":vertical resize -2<CR>")
+map("n", "<C-Right>", ":vertical resize +2<CR>")
+map("n", "<C-Down>", ":resize +2<CR>")
+map("n", "<C-Up>", ":resize -2<CR>")
 
 -- visual模式下缩进代码
-map("v", "<", "<gv", set_opt())
-map("v", ">", ">gv", set_opt())
+map("v", "<", "<gv")
+map("v", ">", ">gv")
 
 -- 上下移动选中文本
-map("v", "J", ":move '>+1<CR>gv-gv", set_opt())
-map("v", "K", ":move '<-2<CR>gv-gv", set_opt())
+map("v", "J", ":move '>+1<CR>gv-gv")
+map("v", "K", ":move '<-2<CR>gv-gv")
