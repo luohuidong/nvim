e70c0beb8b4d785283e2fa6869fa8061e593f6a8
luohuidong <luohuidong01@126.com>
2024-02-24 13:06:34 +0800

    resolve error during tarball extraction

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,9 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
+        -- https://github.com/nvim-treesitter/nvim-treesitter/issues/4250
+        require("nvim-treesitter.install").prefer_git = true
+
         require("nvim-treesitter.configs").setup({
             ensure_installed = {
                 -- Frontend
