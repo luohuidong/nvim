c9d0f6fc695b0a8ebfd6c7b62f60c747dcb327ee
luohuidong <luohuidong01@126.com>
2024-02-27 20:35:24 +0800

    format the code

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,16 +1,22 @@
 -- https://github.com/hrsh7th/nvim-cmp
 
-local lsp_init = require('lsp')
+local lsp_init = require("lsp")
 
 return {
     "hrsh7th/nvim-cmp",
     dependencies = {
-        "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "hrsh7th/cmp-cmdline",
-        "hrsh7th/cmp-vsnip", "hrsh7th/vim-vsnip"
+        --auto complete
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        -- snippets
+        "hrsh7th/cmp-vsnip",
+        "hrsh7th/vim-vsnip",
     },
     config = function()
         -- Set up nvim-cmp.
-        local cmp = require 'cmp'
+        local cmp = require("cmp")
 
         cmp.setup({
             snippet = {
@@ -27,54 +33,53 @@ return {
                 -- documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
-                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
             }),
             sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'vsnip' }, -- For vsnip users.
+                { name = "nvim_lsp" },
+                { name = "vsnip" }, -- For vsnip users.
                 -- { name = 'luasnip' }, -- For luasnip users.
                 -- { name = 'ultisnips' }, -- For ultisnips users.
                 -- { name = 'snippy' }, -- For snippy users.
             }, {
-                { name = 'buffer' },
-            })
+                { name = "buffer" },
+            }),
         })
 
         -- Set configuration for specific filetype.
-        cmp.setup.filetype('gitcommit', {
+        cmp.setup.filetype("gitcommit", {
             sources = cmp.config.sources({
-                { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+                { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
             }, {
-                { name = 'buffer' },
-            })
+                { name = "buffer" },
+            }),
         })
 
         -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline({ '/', '?' }, {
+        cmp.setup.cmdline({ "/", "?" }, {
             mapping = cmp.mapping.preset.cmdline(),
             sources = {
-                { name = 'buffer' }
-            }
+                { name = "buffer" },
+            },
         })
 
         -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline(':', {
+        cmp.setup.cmdline(":", {
             mapping = cmp.mapping.preset.cmdline(),
             sources = cmp.config.sources({
-                { name = 'path' }
+                { name = "path" },
             }, {
-                { name = 'cmdline' }
-            })
+                { name = "cmdline" },
+            }),
         })
 
         -- Set up lspconfig.
-        for _, lsp_init_func in ipairs(lsp_init)
-        do
+        for _, lsp_init_func in ipairs(lsp_init) do
             lsp_init_func()
         end
-    end
+    end,
 }
