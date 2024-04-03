4cf2348c20019b939022bc80d857c870d82d79b7
luohuidong <luohuidong01@126.com>
2024-04-04 02:14:07 +0800

    modify the command of ctrl+s

--- lua/keybindings/formatter-kb.lua
+++ lua/keybindings/formatter-kb.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.basic()
-    vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>Format<cr>", { desc = "Format and save code" })
+    vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>FormatWrite<cr>", { desc = "Format and save code" })
 end
 
 return M
